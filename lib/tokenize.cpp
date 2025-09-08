564bddbe76025da565a7217e92e664c397559fe5
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2025-09-08 11:24:10 +0200

    fix #14118

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -6518,7 +6518,7 @@ void Tokenizer::simplifyHeadersAndUnusedTemplates()
             }
         }
 
-        if (!tok->previous() || Token::Match(tok->previous(), "[;{}]")) {
+        if (!tok->previous() || Token::Match(tok->previous(), "[;{}:]")) {
             // Remove unused function declarations
             if (isIncluded && removeUnusedIncludedFunctions) {
                 while (true) {
